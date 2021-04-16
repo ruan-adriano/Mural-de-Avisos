bfeba5f2af9047589c2c627b61500e59556184ff
Ruan Adriano <prog.ruan@gmail.com>
2021-04-16 19:08:04 -0300

    Base adcionada

---
+++ index.js
@@ -0,0 +1,43 @@
+//Imports of to server
+const express = require('express')
+const app = express()
+const port = 5000
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+//template posts
+let post = [
+    {
+        id: "dfjksdf",
+        title: "Teste de títiulo",
+        descrption: "Teste de descrição"
+    }
+]
+
+
+
+//routers
+app.get('/all', (req, res) => {
+    res.json(JSON.stringify(post))
+})
+
+app.post("/new", bodyParser.json(), (req, res) => {
+
+    let id = generateID();
+    let title = req.body.title;
+    let description = req.body.title;
+
+    function generateID() {
+        return Math.random().toString(36).substr(2,9);
+    }
+
+})
+
+//Server is running
+app.listen(port, () => console.log(`Server is running in the port: ${port}`))
